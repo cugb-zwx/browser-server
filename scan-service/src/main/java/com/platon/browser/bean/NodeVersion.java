8cde163461e37d380764224b3221b310351de7cc
chendongming <chendongming@juzix.net>
2021-09-16 17:14:27 +0800

    fix http://192.168.10.40/bug-view-19918.html

--- scan-service/src/main/java/com/platon/browser/bean/NodeVersion.java
+++ scan-service/src/main/java/com/platon/browser/bean/NodeVersion.java
@@ -3,6 +3,7 @@ package com.platon.browser.bean;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.platon.browser.utils.HexUtil;
 import com.platon.browser.utils.ChainVersionUtil;
+import jnr.ffi.annotations.In;
 
 import java.math.BigInteger;
 
@@ -12,9 +13,14 @@ import java.math.BigInteger;
  * @create: 2019-11-22 10:18:43
  **/
 public class NodeVersion {
+    private Integer programVersion;
+    public Integer getProgramVersion(){
+        return programVersion;
+    }
     @JSONField(name = "ProgramVersion")
     private Integer bigVersion;
     public void setBigVersion(String bigVersion){
+        this.programVersion=Integer.valueOf(bigVersion);
         this.bigVersion = ChainVersionUtil.toBigVersion(new BigInteger(bigVersion)).intValue();
     }
     @JSONField(name = "NodeId")
@@ -26,6 +32,7 @@ public class NodeVersion {
 		return bigVersion;
 	}
 	public void setBigVersion(Integer bigVersion) {
+        this.programVersion=bigVersion;
 		this.bigVersion = ChainVersionUtil.toBigVersion(new BigInteger(String.valueOf(bigVersion))).intValue();
 	}
 	public String getNodeId() {
